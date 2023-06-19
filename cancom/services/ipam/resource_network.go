243ed11514191c3d3f3a45a49eea88ffec9225cc
Clemens Plieth <clemens.plieth@cancom.de>
2023-06-19 09:17:58 +0200

    fixed conflicts, added ipam resources

---
+++ cancom/services/ipam/resource_network.go
@@ -0,0 +1,190 @@
+package ipam
+
+import (
+	"context"
+
+	"github.com/cancom/terraform-provider-cancom/client"
+	client_ipam "github.com/cancom/terraform-provider-cancom/client/services/ipam"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceNetwork() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceNetworkCreate,
+		ReadContext:   resourceNetworkRead,
+		UpdateContext: resourceNetworkUpdate,
+		DeleteContext: resourceNetworkDelete,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"supernet_id": {
+				Type:     schema.TypeString,
+				Computed: false,
+				Required: true,
+			},
+			"name_tag": {
+				Type:     schema.TypeString,
+				Computed: false,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: false,
+				Optional: true,
+			},
+			"request": {
+				Type:     schema.TypeString,
+				Computed: false,
+				Required: true,
+			},
+			"host_assign": {
+				Type:     schema.TypeBool,
+				Computed: false,
+				Optional: true,
+			},
+			"prefix_str": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"updated_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceNetworkRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Client)
+
+	c.HostURL = c.ServiceURLs["ip-management"]
+	var diags diag.Diagnostics
+
+	id := d.Id()
+
+	resp, err := (*client_ipam.Client)(c).GetNetwork(id)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("name_tag", resp.NameTag)
+	d.Set("supernet_id", resp.SupernetId)
+	d.Set("description", resp.Description)
+	d.Set("prefix_str", resp.PrefixStr)
+	d.Set("host_assign", resp.HostAssign)
+	d.Set("created_at", resp.CreatedAt)
+	d.Set("updated_at", resp.UpdatedAt)
+	d.Set("id", resp.ID)
+
+	return diags
+}
+
+func resourceNetworkCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Client)
+
+	c.HostURL = c.ServiceURLs["ip-management"]
+
+	var diags diag.Diagnostics
+
+	network := &client_ipam.NetworkCreateRequest{
+		SupernetId:  d.Get("supernet_id").(string),
+		Request:     d.Get("request").(string),
+		NameTag:     d.Get("name_tag").(string),
+		Description: d.Get("description").(string),
+		HostAssign:  false,
+		//Source:    "CANCOM-TF"  there is a bug in the backend preventing this currently
+	}
+
+	resp, err := (*client_ipam.Client)(c).CreateNetwork(network)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id := resp.ID
+
+	d.SetId(id)
+
+	// if hostAssign is true we must enable ist in an update to th network we just assigned
+	if d.Get("host_assign").(bool) {
+		update := &client_ipam.NetworkUpdateRequest{
+			HostAssign: true,
+			//Source:    "CANCOM-TF",
+		}
+		_, update_err := (*client_ipam.Client)(c).UpdateNetwork(id, update)
+		if update_err != nil {
+			return diag.FromErr(update_err)
+		}
+	}
+
+	resourceNetworkRead(ctx, d, m)
+
+	return diags
+
+}
+
+func resourceNetworkUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Client)
+
+	c.HostURL = c.ServiceURLs["ip-management"]
+
+	var diags diag.Diagnostics
+
+	id := d.Id()
+
+	network := &client_ipam.NetworkUpdateRequest{
+		NameTag:     d.Get("name_tag").(string),
+		Description: d.Get("description").(string),
+		HostAssign:  d.Get("host_assign").(bool),
+		//Source:      "CANCOM-TF",
+	}
+
+	_, err := (*client_ipam.Client)(c).UpdateNetwork(id, network)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(id)
+
+	resourceNetworkRead(ctx, d, m)
+
+	return diags
+}
+
+func resourceNetworkDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Client)
+
+	c.HostURL = c.ServiceURLs["ip-management"]
+
+	var diags diag.Diagnostics
+
+	id := d.Id()
+
+	// first try to disable host assignment if it is enabled.
+	network := &client_ipam.NetworkUpdateRequest{
+		HostAssign: false,
+		//Source:    "CANCOM-TF",
+	}
+	_, err := (*client_ipam.Client)(c).UpdateNetwork(id, network)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = (*client_ipam.Client)(c).DeleteNetwork(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+
+	return diags
+}
--- client/services/ipam/models.go
+++ client/services/ipam/models.go
@@ -1,14 +1,5 @@
 package client_ipam
 
-type Gateway struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
-
-type SupernetCreateRequest struct {
-	Name string `json:"name"`
-}
-
 type Host struct {
 	ID          string `json:"crn,omitempty"`
 	Operation   string `json:"operation"`
@@ -40,4 +31,113 @@ type HostUpdateRequest struct {
 	NameTag     string `json:"nameTag"`
 	Description string `json:"description"`
 	Source      string `json:"source"`
+}
+
+type HostDeleteResponse struct {
+	ID      string `json:"crn,omitempty"`
+	Message string `json:"message"`
+}
+
+type Instance struct {
+	ID              string `json:"crn,omitempty"`
+	Description     string `json:"description"`
+	NameTag         string `json:"nameTag"`
+	ManagedBy       string `json:"managedBy"`
+	ReleaseWaitTime string `json:"releaseWaitTime"`
+	CreatedAt       string `json:"createdAt"`
+	UpdatedAt       string `json:"updatedAt"`
+	Source          string `json:"source"`
+}
+
+type InstanceCreateRequest struct {
+	NameTag         string `json:"nameTag"`
+	ManagedBy       string `json:"managedBy"`
+	Description     string `json:"description"`
+	ReleaseWaitTime string `json:"releaseWaitTime"`
+	Source          string `json:"source"`
+}
+
+type InstanceUpdateRequest struct {
+	NameTag         string `json:"nameTag"`
+	ManagedBy       string `json:"managedBy"`
+	ID              string `json:"crn,omitempty"`
+	Description     string `json:"description"`
+	ReleaseWaitTime string `json:"releaseWaitTime"`
+	Source          string `json:"source"`
+}
+
+type InstanceDeleteResponse struct {
+	ID      string `json:"crn,omitempty"`
+	Message string `json:"message"`
+}
+
+type Supernet struct {
+	ID           string `json:"crn,omitempty"`
+	InstanceId   string `json:"parent"`
+	Description  string `json:"description"`
+	NameTag      string `json:"nameTag"`
+	SupernetCidr string `json:"supernetCidr"`
+	CreatedAt    string `json:"createdAt"`
+	UpdatedAt    string `json:"updatedAt"`
+	Source       string `json:"source"`
+}
+
+type SupernetCreateRequest struct {
+	ID           string `json:"crn,omitempty"`
+	InstanceId   string `json:"parent"`
+	Description  string `json:"description"`
+	NameTag      string `json:"nameTag"`
+	SupernetCidr string `json:"supernetCidr"`
+	Source       string `json:"source"`
+}
+
+type SupernetUpdateRequest struct {
+	ID           string `json:"crn,omitempty"`
+	InstanceId   string `json:"parent"`
+	Description  string `json:"description"`
+	NameTag      string `json:"nameTag"`
+	SupernetCidr string `json:"supernetCidr"`
+	Source       string `json:"source"`
+}
+
+type SupernetDeleteResponse struct {
+	ID      string `json:"crn,omitempty"`
+	Message string `json:"message"`
+}
+
+type Network struct {
+	ID          string `json:"crn,omitempty"`
+	SupernetId  string `json:"parent"`
+	Description string `json:"description"`
+	NameTag     string `json:"nameTag"`
+	PrefixStr   string `json:"prefix_str"`
+	HostAssign  bool   `json:"hostAssign"`
+	CreatedAt   string `json:"createdAt"`
+	UpdatedAt   string `json:"updatedAt"`
+	Source      string `json:"source"`
+	//HostAssign   bool	`json:hostAssign`
+}
+
+type NetworkCreateRequest struct {
+	ID          string `json:"crn,omitempty"`
+	SupernetId  string `json:"supernetCrn"`
+	Request     string `json:"request"`
+	Description string `json:"description"`
+	NameTag     string `json:"nameTag"`
+	HostAssign  bool   `json:"hostAssign"`
+	//Source      string `json:"source"`
+	//HostAssign   bool	`json:hostAssign`
+}
+
+type NetworkUpdateRequest struct {
+	ID          string `json:"crn,omitempty"`
+	Description string `json:"description"`
+	NameTag     string `json:"nameTag"`
+	//Source      string `json:"source"`
+	HostAssign bool `json:"hostAssign"`
+}
+
+type NetworkDeleteResponse struct {
+	ID      string `json:"crn,omitempty"`
+	Message string `json:"message"`
 }
