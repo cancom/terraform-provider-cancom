97d041714f5db087706370241b2833f09ac6ea61
Benedict Becker <benedict.becker@cancom.de>
2023-06-19 12:43:08 +0200

    feat: adds ipam resources - supernet, network, instance

--- client/services/ipam/ipam.go
+++ client/services/ipam/ipam.go
@@ -3,6 +3,7 @@ package client_ipam
 import (
 	//"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/rand"
 	"net/http"
@@ -14,10 +15,329 @@ import (
 
 type Client client.Client
 
+// -----------------------------Network---------------------------------
+
+// GetNetwork - Returns a specifc Network
+func (c *Client) GetNetwork(networkId string) (*Network, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/v1/Networks/%s", c.HostURL, networkId), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	network := Network{}
+	err = json.Unmarshal(body, &network)
+	if err != nil {
+		return nil, err
+	}
+
+	return &network, nil
+}
+
+// CreateNetwork - Assign a new network from a given supernet or supernetPool
+func (c *Client) CreateNetwork(network *NetworkCreateRequest) (*Network, error) {
+	rb, err := json.Marshal(network)
+	if err != nil {
+		return nil, err
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(5) // n will be between 0 and 10
+	time.Sleep(time.Duration(n) * time.Second)
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/v1/Networks/assign", c.HostURL), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	newNetwork := Network{}
+	err = json.Unmarshal(body, &newNetwork)
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(4 * time.Second)
+	return &newNetwork, nil
+
+}
+
+// UpdateNetwork - Update a network
+func (c *Client) UpdateNetwork(networkId string, network *NetworkUpdateRequest) (*Network, error) {
+	rb, err := json.Marshal(network)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/Networks/%s", c.HostURL, networkId), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	newNetwork := Network{}
+	err = json.Unmarshal(body, &newNetwork)
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(4 * time.Second)
+	return &newNetwork, nil
+}
+
+// DeleteNetwork - Deleting a network releases it to the pool
+func (c *Client) DeleteNetwork(networkId string) error {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/v1/Networks/release/%s", c.HostURL, networkId), nil)
+	if err != nil {
+		return err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return err
+	}
+
+	delResponse := NetworkDeleteResponse{}
+	err = json.Unmarshal(body, &delResponse)
+	if err != nil {
+		return err
+	}
+
+	//if delResponse.Message != "released successfully" {
+	//	return errors.New("message: " + delResponse.Message)
+	//}
+
+	return nil
+}
+
+// -----------------------------Supernet---------------------------------
+
+// GetSupernet - Returns a specifc Supernet
+func (c *Client) GetSupernet(supernetId string) (*Supernet, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/v1/Networks/%s", c.HostURL, supernetId), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	supernet := Supernet{}
+	err = json.Unmarshal(body, &supernet)
+	if err != nil {
+		return nil, err
+	}
+
+	return &supernet, nil
+}
+
+// CreateSupernet - Create new supernet
+func (c *Client) CreateSupernet(supernet *SupernetCreateRequest) (*Supernet, error) {
+	rb, err := json.Marshal(supernet)
+	if err != nil {
+		return nil, err
+	}
+
+	//return nil, errors.New("message create supernet")
+	// sleep for a random time to resolve potential race condition problem
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(5) // n will be between 0 and 10
+	time.Sleep(time.Duration(n) * time.Second)
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/v1/Networks", c.HostURL), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	newSupernet := Supernet{}
+	err = json.Unmarshal(body, &newSupernet)
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(4 * time.Second)
+	return &newSupernet, nil
+
+}
+
+// UpdateSupernet - Updates a supernet
+func (c *Client) UpdateSupernet(supernetId string, supernet *SupernetUpdateRequest) (*Supernet, error) {
+	rb, err := json.Marshal(supernet)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/Networks/%s", c.HostURL, supernetId), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	newSupernet := Supernet{}
+	err = json.Unmarshal(body, &newSupernet)
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(4 * time.Second)
+	return &newSupernet, nil
+}
+
+// DeleteSupernet - Delete a Supernet
+func (c *Client) DeleteSupernet(supernetId string) error {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/v1/Networks/%s", c.HostURL, supernetId), nil)
+	if err != nil {
+		return err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return err
+	}
+
+	delResponse := SupernetDeleteResponse{}
+	err = json.Unmarshal(body, &delResponse)
+	if err != nil {
+		return err
+	}
+
+	//if delResponse.Message != "released successfully" {
+	//	return errors.New("message: " + delResponse.Message)
+	//}
+
+	return nil
+}
+
+// -----------------------------Instance---------------------------------
+
+// GetInstance - Returns a specifc Instance
+func (c *Client) GetInstance(instanceId string) (*Instance, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/v1/Instances/%s", c.HostURL, instanceId), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	instance := Instance{}
+	err = json.Unmarshal(body, &instance)
+	if err != nil {
+		return nil, err
+	}
+
+	return &instance, nil
+}
+
+// CreateInstance - Create new instance
+func (c *Client) CreateInstance(instance *InstanceCreateRequest) (*Instance, error) {
+	rb, err := json.Marshal(instance)
+	if err != nil {
+		return nil, err
+	}
+	// sleep for a random time to resolve potential race condition problem
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(5) // n will be between 0 and 10
+	time.Sleep(time.Duration(n) * time.Second)
+
+	//return nil, errors.New("message: " + c.HostURL)  //for debugging
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/v1/Instances", c.HostURL), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	newInstance := Instance{}
+	err = json.Unmarshal(body, &newInstance)
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(4 * time.Second)
+	return &newInstance, nil
+
+}
+
+// UpdateInstance - Updates an instance
+func (c *Client) UpdateInstance(instanceId string, instance *InstanceUpdateRequest) (*Instance, error) {
+	rb, err := json.Marshal(instance)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/v1/Instances/%s", c.HostURL, instanceId), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	newInstance := Instance{}
+	err = json.Unmarshal(body, &newInstance)
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(4 * time.Second)
+	return &newInstance, nil
+}
+
+// DeleteInstance - Delete an Instance
+func (c *Client) DeleteInstance(instanceId string) error {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/v1/Instances/%s", c.HostURL, instanceId), nil)
+	if err != nil {
+		return err
+	}
+
+	body, err := (*client.Client)(c).DoRequest(req)
+	if err != nil {
+		return err
+	}
+
+	delResponse := InstanceDeleteResponse{}
+	err = json.Unmarshal(body, &delResponse)
+	if err != nil {
+		return err
+	}
+
+	//if delResponse.Message != "released successfully" {
+	//	return errors.New("message: " + delResponse.Message)
+	//}
+
+	return nil
+}
+
+// -------------------------------------Host--------------------------------------------------
+
 // GetHost - Returns a specifc host address
 func (c *Client) GetHost(hostId string) (*Host, error) {
-	//return nil, errors.New(c.HostURL)
-
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/v1/Hosts/%s", c.HostURL, hostId), nil)
 	if err != nil {
 		return nil, err
@@ -105,8 +425,14 @@ func (c *Client) DeleteHost(hostId string) error {
 		return err
 	}
 
-	if string(body) != "Deleted host" {
-		return nil //errors.New(string(body))
+	delResponse := HostDeleteResponse{}
+	err = json.Unmarshal(body, &delResponse)
+	if err != nil {
+		return err
+	}
+	// included to find a sporadic problem leading to resources not released in the backend
+	if delResponse.Message != "released successfully" {
+		return errors.New("message: " + delResponse.Message)
 	}
 
 	return nil
--- examples/services/ipam/resources/ipam_supernet.tf
+++ examples/services/ipam/resources/ipam_supernet.tf
@@ -1,8 +1,16 @@
 
-resource "cancom_ipam_host" "host" {
-	network_crn = "crn:KmBRNWt87RV26jJfZMeJus::ip-management:network:F4tfeFwx2i7HFjNrFop49z"
-   	name_tag = "hostname.cancom.de"
-   	qualifier = "<qualifier, optional>"
-   	description = "<description for this host-assignment>"
+resource "cancom_ipam_supernet" "<name of this resource>" {
+  instance_id = "<id of the managed instance this supernet shall belong to>"
+  name_tag = "<name>"
+  supernet_cidr = "<the prefix in cidr notation>"
+  description = "<a description>"
+}
+
+# example with id from instance within same stack
+resource "cancom_ipam_supernet" "tfsupernet01" {
+  instance_id = cancom_ipam_instance.tfcreated01.id
+  name_tag = "supernet-from-tf"
+  supernet_cidr = "10.100.0.0/16"
+  description = "supernet created by terraform"
 }
 
