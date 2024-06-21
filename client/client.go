a803efbe9c324d30fcc35202ba4cd75c467ba83f
Benedict Becker <benedict.becker@cancom.de>
2024-06-21 09:17:22 +0200

    feat: adds retry to dns record crud

--- client/client.go
+++ client/client.go
@@ -1,8 +1,10 @@
 package client
 
 import (
+	"bytes"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -45,8 +47,9 @@ func (c *Client) DoRequest(req *http.Request) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
@@ -56,4 +59,58 @@ func (c *Client) DoRequest(req *http.Request) ([]byte, error) {
 	}
 
 	return body, nil
+}
+
+func (c *Client) DoRequestWithRetry(req *http.Request, policy func(resp *http.Response) bool) ([]byte, error) {
+	token := c.Token
+	if policy == nil {
+		policy = func(resp *http.Response) bool {
+			return resp.StatusCode == 429
+		}
+	}
+
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	var bodyGetter = func() io.ReadCloser {
+		return http.NoBody
+	}
+
+	if req.Body != nil {
+		buf, err := io.ReadAll(req.Body)
+		if err != nil {
+			return nil, err
+		}
+		defer req.Body.Close()
+
+		bodyGetter = func() io.ReadCloser {
+			return io.NopCloser(bytes.NewReader(buf))
+		}
+	}
+
+	for i := 0; i < 30; i++ {
+		req.Body = bodyGetter()
+		resp, err := c.HTTPClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		if policy(resp) {
+			time.Sleep(time.Duration(rand.Intn(16)+15) * time.Second)
+			continue
+		}
+
+		if (resp.StatusCode != http.StatusOK) && (resp.StatusCode != 201) {
+			return nil, fmt.Errorf("status: %d, body: %s", resp.StatusCode, body)
+		}
+
+		return body, nil
+	}
+
+	return nil, fmt.Errorf("maximum retries has been reached")
 }
