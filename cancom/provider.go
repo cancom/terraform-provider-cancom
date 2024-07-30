dcd110af42cdb13914abb7771f0952d66b1100b2
Benedict Becker <benedict.becker@cancom.de>
2024-07-30 16:35:13 +0200

    fix: readd assume role workflow

--- cancom/provider.go
+++ cancom/provider.go
@@ -2,12 +2,15 @@ package cancom
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/cancom/terraform-provider-cancom/client"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+
+	client_iam "github.com/cancom/terraform-provider-cancom/client/services/iam"
 )
 
 func Provider() *schema.Provider {
@@ -45,7 +48,7 @@ func Provider() *schema.Provider {
 func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
 	token := d.Get("token").(string)
 	service_registry := d.Get("service_registry").(string)
-	// role := d.Get("role").(string)
+	role := d.Get("role").(string)
 
 	// Warning or errors can be collected in a slice type
 	var diags diag.Diagnostics
@@ -61,24 +64,29 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
 			return nil, diags
 		}
 
-		// if role != "" {
-		// 	c.HostURL = c.ServiceURLs["iam"]
+		if role != "" {
+			iamService, err := c.GetService("iam")
 
-		// 	token, err := (*client_iam.Client)(c).AssumeRole(&client_iam.AssumeRoleRequest{
-		// 		Role: role,
-		// 	})
+			if err != nil {
+				return nil, diag.FromErr(err)
+			}
 
-		// 	if err != nil {
-		// 		diags = append(diags, diag.Diagnostic{
-		// 			Severity: diag.Error,
-		// 			Summary:  fmt.Sprintf("Could not assume role %s", role),
-		// 			Detail:   err.Error(),
-		// 		})
-		// 		return nil, diags
-		// 	}
+			token, err := (*client_iam.Client)(iamService).AssumeRole(&client_iam.AssumeRoleRequest{
+				Role: role,
+			})
 
-		// 	c.Token = token.Jwt
-		// }
+			if err != nil {
+				diags = append(diags, diag.Diagnostic{
+					Severity: diag.Error,
+					Summary:  fmt.Sprintf("Could not assume role %s", role),
+					Detail:   err.Error(),
+				})
+				return nil, diags
+			}
+
+			// set and update used token
+			c = c.WithToken(token.Jwt)
+		}
 
 		return c, diags
 	}
