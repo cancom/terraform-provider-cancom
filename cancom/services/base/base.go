016ff418592e278700da194a61c46433c4e42abe
Clemens Röhl <33691485+roehlc@users.noreply.github.com>
2024-11-13 08:03:29 +0000

    chore: go fmt

--- cancom/services/base/base.go
+++ cancom/services/base/base.go
@@ -3,7 +3,7 @@ package base
 import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 type Provider struct {
-	ServiceName string
+	ServiceName    string
 	ProviderSchema *schema.Provider
 }
 
