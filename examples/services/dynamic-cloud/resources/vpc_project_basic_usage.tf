8e8e461aede548b05ebe8a183a2ff46a5db17f81
Clemens Röhl <33691485+roehlc@users.noreply.github.com>
2024-11-15 07:52:22 +0000

    feat/examples: add dynamic-cloud basic usage

---
+++ examples/services/dynamic-cloud/resources/vpc_project_basic_usage.tf
@@ -0,0 +1,4 @@
+resource "cancom_dynamic_cloud_vpc_project" "basic_usage_example" {
+  name            = "test-cancom-terraform-provider"
+  project_comment = "Test VPC Project created with the CANCOM Terraform provider"
+}
